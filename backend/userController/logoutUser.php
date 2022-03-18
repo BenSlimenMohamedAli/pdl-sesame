b23a9a42741deafe43dcd5e6b0b01849b32ad73e
minercool <zghidi@gmail.com>
2022-03-18 19:04:37 +0100

    updates

--- backend/userController/logoutUser.php
+++ backend/userController/logoutUser.php
@@ -1,5 +1,5 @@
 <?php
-    require_once '../config.php';
+require_once '../config.php';
 if(isset($_SESSION['logged']) && $_SESSION['logged'] == true){
     session_destroy();
     header('Location: ../../LMS/login.php');
